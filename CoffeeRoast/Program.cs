0e74dff3ea0a8cce82df3c0d333b03d29958d450
Beluchini <pavelg25052007@gmail.com>
2025-09-06 14:49:55 +0400

    C Home закончено, переходу к BasketPage  и начинаю делать заказ

--- CoffeeRoast/Program.cs
+++ CoffeeRoast/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<CoffeeContext>();
 builder.Services.AddScoped<ProtectedLocalStorage>();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
